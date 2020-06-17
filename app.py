2007f28ecfecf4014b8416632c3694cd0a913270
Qi <784359442@qq.com>
2020-06-16 17:00:21 -0700

    Create app skeleton

--- app.py
+++ app.py
@@ -2,15 +2,24 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
 
-  return app
+    @app.route('/')
+    def test():
+        return 'Hello World'
+
+    return app
+
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    # APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run()
